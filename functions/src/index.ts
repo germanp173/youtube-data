97bcbc62b6097e23b9f7895b3053617395aacb10
German Pineda <germanp173@gmail.com>
2019-08-06 17:15:14 -0700

    Transfer get views logic to firebase function and add all necessary dependencies

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,17 @@
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
+import * as cheerio from "cheerio";
+import axios from "axios";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+export const GetViews = functions.https.onRequest((request, response) => {
+  console.log(JSON.stringify(request.body));
+  axios
+    .get("https://www.youtube.com/watch?v=F81cLjRY4to")
+    .then(res => {
+      const $ = cheerio.load(res.data);
+      const views = $(".watch-view-count")[0].children[0].data;
+
+      console.log(views);
+      response.send(views);
+    })
+    .catch(err => console.log(err));
+});
--- src/index.js
+++
@@ -1,9 +0,0 @@
-const axios = require("axios");
-const cheerio = require("cheerio");
-
-axios.get("https://www.youtube.com/watch?v=F81cLjRY4to").then(response => {
-  const $ = cheerio.load(response.data);
-  const views = $(".watch-view-count")[0].children[0].data;
-
-  console.log(views);
-});
