4b8e911f8346ea1c1fb83f8d8e8fa9d1165dee9c
German Pineda <germanp173@gmail.com>
2020-01-07 15:58:40 -0800

    Add console log to debug 500 crash errors

--- functions/src/getViews.ts
+++ functions/src/getViews.ts
@@ -9,13 +9,21 @@ export const getViews = async (
   request: functions.https.Request,
   response: functions.Response
 ) => {
+
+  console.log("Starting here!");
+
   // Ensure urls array is provided
   const urls: string[] = request.body.urls;
+
+  console.log("urls: ", urls);
+
   if (!urls && Array.isArray(urls)) {
     response.statusCode = 400;
     response.send("No URLs array provided");
     return;
   }
+
+  console.log("Starting promise map");
 
   // Create array of promises.
   // Each array item is a promise that will retrieve the views
@@ -24,9 +32,11 @@ export const getViews = async (
 
   // Returns all views after all promises are resolved.
   try {
+    console.log("Invoking all promises");
     const videoData = await Promise.all(promises);
+    console.log("Done with all promises");
     const returnHash: ReturnHashMap = {};
-    
+
     videoData.forEach(video => (returnHash[video.url] = video.views));
     response.statusCode = 200;
     response.send(returnHash);
