51ec2865d719ab2e6419e3a2b20383355a7e9165
German Pineda <germanp173@gmail.com>
2020-01-07 13:38:46 -0800

    Extend function timeout

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,6 +2,7 @@ import * as functions from "firebase-functions";
 import { getViews } from "./getViews";
 
 const runtimeOpts: functions.RuntimeOptions = {
+  timeoutSeconds: 300,
   memory: "2GB"
 }
 
