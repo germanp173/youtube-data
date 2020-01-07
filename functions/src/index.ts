1edaacdcf32573d62ce3ef9b5d0dfe625c59b25d
German Pineda <germanp173@gmail.com>
2020-01-07 12:10:33 -0800

    Expand cloud function memmory allocation to 2 gbs

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,10 @@
 import * as functions from "firebase-functions";
 import { getViews } from "./getViews";
 
+const runtimeOpts: functions.RuntimeOptions = {
+  memory: "2GB"
+}
+
 module.exports = {
-  getViews: functions.https.onRequest(getViews)
+  getViews: functions.runWith(runtimeOpts).https.onRequest(getViews)
 };
